d2b73152200dbe89daf0d159e26e17cccd49c073
Lalit Singh <nlalitsingh2014@gmail.com>
2026-09-06 13:22:51 +0545

    Add browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/Themecontext.jsx'
 
 createRoot(document.getElementById('root')).render(
  
-    <HashRouter>
+    <BrowserRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   
 )
